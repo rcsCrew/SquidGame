da4028b278b8643b21d314d37bd943f17f13fb6d
ramon senger <51542841+rcsCrew@users.noreply.github.com>
2024-08-29 14:28:24 -0300

    30%

---
+++ squidgame/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HubComponent } from './pages/hub/hub.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HubComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
